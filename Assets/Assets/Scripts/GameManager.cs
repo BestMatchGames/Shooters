b73bf3618327e68a628235ac6f6d34f1a75a7ba1
BestMatchGames <131631593+BestMatchGames@users.noreply.github.com>
2024-11-03 19:43:26 -0500

    Initial commit

---
+++ Assets/Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject player;
+    public GameObject enemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(player, transform.position, Quaternion.identity);
+        InvokeRepeating("CreateEnemy", 1f, 3f);
+        InvokeRepeating("CreateEnemy", 5f, 4f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CreateEnemy()
+    {
+        Instantiate(enemy, new Vector3(Random.Range(-9f, 9f), 9f, 0), Quaternion.identity);
+    }
+}
